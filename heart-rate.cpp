c997dbc7e331edaf39033819c93a311de583077f
Lori Hogan <lori.hogan@gmail.com>
2019-03-26 09:24:46 -0230

    Fix compile errors

--- heart-rate.cpp
+++ heart-rate.cpp
@@ -6,9 +6,50 @@
 int sampleSize;
 unsigned long hrIntervals[100];
 int hrSamples;
+int hrSampleCount;
 unsigned long hrLastBeat;
 bool hrIntervalsReady=false;
 bool hrReset=false;
+
+/*Function: Interrupt service routine.Get the sigal from the external interrupt*/
+void interrupt()
+{
+	unsigned long interval = millis()- hrLastBeat;
+	
+	if(interval > 2000)//set 2 seconds as max heart pluse duty
+	{
+		Serial.println("Reset");
+		hrReset=true;//need to reset
+		hrIntervalsReady = false;
+		hrSampleCount=0;
+		for(int i=0;i < hrSamples;i ++)
+		{
+			hrIntervals[i]=0;
+		}
+		hrLastBeat = millis();
+	}
+	else if (hrSampleCount < hrSamples) //Need more samples
+	{
+	  Serial.println("More samples");
+	  hrReset=false;
+	  hrIntervalsReady=false;
+	  hrIntervals[hrSampleCount] = interval;
+	  hrLastBeat = millis();
+	  hrSampleCount++;
+	}
+	else //Have required samples, need to shift samples in array
+	{
+		Serial.println("Have samples");
+		hrIntervalsReady = false;
+		for(unsigned char i=1;i < hrSamples;i ++)
+		{
+			hrIntervals[i-1]=hrIntervals[i];
+		}
+		hrIntervals[hrSamples-1] = interval;
+		hrLastBeat = millis();
+		hrIntervalsReady = true;
+	}
+}
 
 
 /**
@@ -16,13 +57,16 @@ Initialises HR monitor so it can be used - call in initProject()
 */
 void hrInit(int inputPort, int sampleCount)
 {
+	Serial.begin(9600);
 	hrSamples = sampleCount;
-	attachInterrupt(digitalPinToInterrupt (inputPort), interrupt, RISING);//set interrupt 0,digital port 2
+	attachInterrupt(digitalPinToInterrupt(inputPort), interrupt, RISING);//set interrupt 0,digital port 2
 	for(int i=0;i < hrSamples;i ++)
     {
         hrIntervals[i]=0;
     }
 	hrLastBeat = millis();
+	
+	Serial.println("Setup complete");
 }
 
 /**
@@ -37,36 +81,3 @@ bool hrGetIntervals(unsigned long intervals[])
 }
 
 
-/*Function: Interrupt service routine.Get the sigal from the external interrupt*/
-void interrupt()
-{
-	unsigned long interval = millis()- hrLastBeat;
-	
-	if(interval > HR_RESET_TIME)//set 2 seconds as max heart pluse duty
-	{
-		hrReset=true;//need to reset
-		hrIntervalsReady = false;
-		hrSampleCount=0;
-		for(int i=0;i < hrSamples;i ++)
-		{
-			hrIntervals[i]=0;
-		}
-	}
-	else if (hrSampleCount < hrSamples) //Need more samples
-	{
-	  hrReset=false;
-	  hrIntervalsReady=false;
-	  hrIntervals[hrSampleCount] = interval;
-	  hrSampleCount++;
-	}
-	else //Have required samples, need to shift samples in array
-	{
-		hrIntervalsReady = false;
-		for(unsigned char i=1;i < hrSamples;i ++)
-		{
-			hrIntervals[i-1]=hrIntervals[i];
-		}
-		hrIntervals[hrSamples-1] = interval;
-		hrIntervalsReady = true;
-	}
-}
